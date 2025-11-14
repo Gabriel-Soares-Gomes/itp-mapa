88f2806777631968e5e3f5b0de1de0fbb4a3cb98
Soares <gabrielsoaresgomes14@gmail.com>
2025-11-14 11:29:43 -0300

    now matriz class implementation

--- proj-itp/src/imagem/imagem.h
+++ proj-itp/src/imagem/imagem.h
@@ -5,6 +5,18 @@ struct Pixel{
     int r, g, b;
 };
 
+class Matriz{
+    int largura, altura; //capacidade
+    int linhas, colunas; //tamanho
+
+    public:
+
+    int obterTamanho(){
+        return (linhas*colunas);
+    }
+
+};
+
 class Imagem{
     int largura, altura;
     Sequencia<Pixel> pixels;

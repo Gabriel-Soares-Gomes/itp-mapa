98d75e4631064b89378d87cb345c4054e0708838
Marcus <marcuslimalsza@gmail.com>
2025-11-30 19:05:28 -0300

    adicionando testes para as matrizes

--- proj-itp/src/terreno/terreno.h
+++ proj-itp/src/terreno/terreno.h
@@ -1,86 +1,129 @@
 #pragma once
-#include <cstdlib>
-#include <cmath>
-#include <ctime>
-#include <iostream>
-#include "../imagem/imagem.h"
 #include "../../../doctest.h"
+#include "../imagem/imagem.h"
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <iostream>
+const float rugosidade = 0.5;
 
-class Terreno {
+class Terreno{
+
     Matriz<int> *terreno;
-    int medida;
-    
-    void geradorCantosIniciais()
-    {
-        //Define os valores aleatórios dos 4 cantos do terreno;
-        srand(time(0));
-        
-        //Intervalo de alturas: [1, 10]
-        (*this)(0,0) = rand()%10+1;
-        (*this)(0, medida-1) = rand()%10+1;
-        (*this)(medida-1, 0) = rand()%10+1;
-        (*this)(medida-1, medida-1) = rand()%10+1;
-    }
-
-    void DiamondSquare()
-    {
-        diamondStep();
-        squareStep();
-    }
-
-
-    void diamondStep()
-    {
-        
-    }
-    
-    void squareStep()
-    {
-
-    }
-    
+    int dimensao;
     public:
 
-    Terreno(int n)
-    {
-        medida = pow(2, n) + 1;
-        //Cria novo terreno quadrado de tamanho 2^n + 1
-        terreno = new Matriz<int>(medida,medida);
-        
-        geradorCantosIniciais();
-        DiamondSquare();
+    Terreno(int n){
+        dimensao =  pow(2, n) + 1;
+        terreno = new Matriz<int>(dimensao, dimensao);
+        for(int i = 0; i < (dimensao*dimensao); i++){
+            terreno->obterElemento(i) = 0;
+        }
+        //srand(time(0));
+        matrizInicial();
     }
 
-    ~Terreno() 
-    {
+    ~Terreno() {
         delete terreno;
     }
 
-    int obterLargura()
-    {
+    int& operator()(int linha, int coluna){
+        int indice = ((linha*dimensao) + coluna);
+        return terreno->obterElemento(indice);
+    }
+
+    void matrizInicial(){
+        (*this)(0, 0) = rand() % 10; //perguntar a André por que o *this.
+        (*this)(0, dimensao - 1) = rand() % 10;
+        (*this)(dimensao - 1, 0) = rand() % 10;
+        (*this)((dimensao - 1), (dimensao - 1)) = rand() % 10;
+        DiamondSquare();
+    }
+
+    void imprimirMatriz(){
+        for(int i = 0; i < terreno->obterAltura(); i++){
+            for(int j = 0; j < terreno->obterLargura(); j++){
+                std::cout << (*this)(i, j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void DiamondSquare(){
+        int passos = (dimensao-1)/2;
+        int variancia = dimensao/2;
+
+        while(passos >= 1){
+            Diamond(passos, variancia);
+            Square(passos, variancia);
+
+            passos /= 2;
+            variancia = variancia*rugosidade;
+        }
+    }
+
+    void Diamond(int passos, int variancia){
+        int media;
+        int deslocamento;
+        //laço percorrendo os cantos esquerdos superiores de cada quadrado
+        for(int linha = 0; linha < dimensao-1; linha += 2*passos){
+            for(int coluna = 0; coluna < dimensao-1; coluna += 2*passos){
+                media = ((*this)(linha, coluna) + (*this)(linha, coluna+2*passos) + (*this)(linha+2*passos, coluna) + (*this)(linha+2*passos, coluna+2*passos))/4;
+                deslocamento = (rand() % ((2*variancia) + 1)) - variancia;
+                (*this)(linha+passos, coluna+passos) = media + deslocamento;
+            }
+        }
+    }
+
+    void Square(int passos, int variancia){
+        int colunaInicial;
+        int deslocamento;
+        int media;
+        
+        for(int linha = 0; linha <= dimensao-1; linha += passos){
+            //verifica se estamos na linha do ponto central.
+            if((linha%(2*passos)) == 0){
+                colunaInicial = passos; 
+            }
+            else{
+                colunaInicial = 0;
+            }
+
+
+            for(int coluna = colunaInicial; coluna <= dimensao-1; coluna += 2*passos){
+                int soma = 0;
+                int qtd = 0;
+                //verificar se os pontos estão dentro ou fora da matriz:
+                if((coluna-passos) >= 0){
+                    soma += (*this)(linha, coluna-passos);
+                    qtd++;
+                }
+                if((coluna+passos) <= (dimensao-1)){
+                    soma += (*this)(linha, coluna+passos);
+                    qtd++;
+                }
+                if((linha-passos) >= 0){
+                    soma += (*this)(linha-passos, coluna);
+                    qtd++;
+                }
+                if((linha+passos) <= (dimensao-1)){
+                    soma += (*this)(linha+passos, coluna);
+                    qtd++;
+                }
+
+                media = soma/qtd;
+                deslocamento = (rand() % ((2*variancia) + 1)) - variancia;
+
+                (*this)(linha, coluna) = media + deslocamento;
+            }
+        }
+    }
+
+    int obterLargura(){
         return terreno->obterLargura();
     }
 
-    int obterProfundidade()
-    {
+    int obterProfundidade(){
         return terreno->obterAltura();
     }
-
-    int& operator() (int lin, int col)
-    {
-        //Retorna uma célula específica da matriz terreno(l, c)
-        return terreno->obterElemento((lin*medida)+col);
-    }
-
-    void printMat()
-    {
-        for(int i = 0; i < medida; i++)
-        {
-            for(int j = 0; j < medida; j++)
-            {
-                std::cout << this->operator()(i,j) << ' ';
-            }
-            std::cout << '\n';
-        }
-    }
-};                          
+};
